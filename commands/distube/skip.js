f1022e9f1f91faa3390cd1a5620d36b6f121f55b
MohammedQht1902 <alkhalgaming78@gmail.com>
2025-02-10 18:28:28 +0300

    Update skip.js

--- commands/distube/skip.js
+++ commands/distube/skip.js
@@ -67,7 +67,7 @@ module.exports = {
                     .setAuthor({ 
                         name: lang.skipNoQueueTitle, 
                         iconURL: musicIcons.wrongIcon,
-                        url: "https://discord.gg/xQF9f9yUEM"
+                        url: "https://discord.gg/SP2JAy6r8T"
                     })
                     .setFooter({ text: 'Distube Player', iconURL: musicIcons.footerIcon })  
                     .setDescription(lang.skipNoQueueMessage);
@@ -79,7 +79,7 @@ module.exports = {
                     .setAuthor({ 
                         name: lang.skipNoUpNextTitle, 
                         iconURL: musicIcons.wrongIcon,
-                        url: "https://discord.gg/xQF9f9yUEM"
+                        url: "https://discord.gg/SP2JAy6r8T"
                     })
                     .setFooter({ text: 'Distube Player', iconURL: musicIcons.footerIcon })  
                     .setDescription(lang.skipNoUpNextMessage);
