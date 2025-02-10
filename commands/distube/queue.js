c0d7a8b6e10ebeea1ca3c8f1d4c8af5f4b6f1cff
MohammedQht1902 <alkhalgaming78@gmail.com>
2025-02-10 18:29:37 +0300

    Update queue.js

--- commands/distube/queue.js
+++ commands/distube/queue.js
@@ -22,7 +22,7 @@ module.exports = {
                 .setAuthor({ 
                     name: lang.queueNoSongsTitle, 
                     iconURL: musicIcons.wrongIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/SP2JAy6r8T"
                 })
                 .setFooter({ text: 'Distube Player', iconURL: musicIcons.footerIcon })   
                 .setDescription(lang.queueNoSongsMessage);
